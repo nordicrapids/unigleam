0338592be6f8ada728159b567e6b1e44da2887b7
Vincent <vincentlkl@gmail.com>
2016-06-10 20:14:58 +0800

    added geocoder

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -2,7 +2,8 @@ class Admin::PagesController < ApplicationController
   layout "admin"
 
   def dashboard
-    @survey_responses = SurveyResponse.group_by_day(:created_at, format: "%Y-%m-%d").order("day asc").count    
+    @survey_responses = SurveyResponse.group_by_day(:created_at, format: "%Y-%m-%d").order("day asc").count
+    @users = User.all    
   end
 
 end
