2cf4baa0c5277ee04f93a518b342c6a97256282e
Vincent <vincentlkl@gmail.com>
2016-06-10 19:46:09 +0800

    added chart

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -2,7 +2,7 @@ class Admin::PagesController < ApplicationController
   layout "admin"
 
   def dashboard
-
+    @survey_responses = SurveyResponse.group_by_day(:created_at, format: "%Y-%m-%d").order("day asc").count    
   end
 
 end
