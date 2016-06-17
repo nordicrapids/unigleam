e59c0d07aee8325908ea5fd334c06fd0847d2667
Vincent <vincentlkl@gmail.com>
2016-06-17 20:45:17 +0800

    added main js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,5 +15,15 @@
 //= require twitter/bootstrap
 //= require highcharts
 //= require theme/js/royal_preloader.min
+//= require theme/js/jquery.easing.min
+//= require theme/js/jquery.mixitup.min
 //= require theme/js/scrollReveal
+//= require theme/js/owl.carousel.min
+//= require theme/js/jquery.magnific-popup.min
+//= require theme/js/jquery.snippet.min
+//= require theme/js/jquery.fitvids
+//= require theme/js/style-switcher
+//= require theme/js/activate-snippet
+//= require theme/js/skrollr.min
+//= require theme/js/main
 //= require_tree .
