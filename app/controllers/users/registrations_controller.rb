a3522d41c292a53d44875b7415967a7b983cb3fb
Vincent <vincentlkl@gmail.com>
2016-06-21 13:45:07 +0800

    added pop up user registration

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,24 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+
+  def create
+    build_resource(sign_up_params)
+
+    if resource.save
+      sign_up(resource_name, resource)
+
+    else
+      @error = resource.errors.full_messages.to_sentence
+    end
+  end
+
+  # Signs in a user on sign up. You can overwrite this method in your own
+  # RegistrationsController.
+  def sign_up(resource_name, resource)
+    sign_in(resource_name, resource)
+  end
+
+  def sign_up_params
+    devise_parameter_sanitizer.sanitize(:sign_up)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { :sessions => 'users/sessions', :registrations => "registrations", :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :sessions => 'users/sessions', :registrations => "users/registrations", :omniauth_callbacks => "users/omniauth_callbacks" }
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
