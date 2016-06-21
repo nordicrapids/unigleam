7b9bf903ee1d9eeca54a401061f5dd1f6739522d
Vincent <vincentlkl@gmail.com>
2016-06-21 18:29:04 +0800

    tmp commit

--- db/migrate/20160621063611_add_image_to_survey_question.rb
+++ db/migrate/20160621063611_add_image_to_survey_question.rb
@@ -1,6 +1,6 @@
 class AddImageToSurveyQuestion < ActiveRecord::Migration
   def change
     add_attachment :survey_questions, :image
-    add_column :survey_questions, :slug, :string
+    # add_column :survey_questions, :slug, :string
   end
 end
