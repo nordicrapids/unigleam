5cd67962d0e3726fcc7475428b889d33120b7280
dharin <dharin.rajgor@gmail.com>
2016-11-22 10:09:06 +0530

    set the setting for smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,8 +54,8 @@ Rails.application.configure do
     domain: "google.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: "dharin.test@gmail.com",
-    password: "cac5ee05a3850e03"
+    user_name: "info@unigleam.com",
+    password: "Sneakerchange2016"
   }
 
   config.action_mailer.default_url_options = { :host => 'https://unigleam-development.herokuapp.com/' }
