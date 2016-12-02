16eb95548edd63c72dc69424d6954d054bbd96ad
dharin <dharin.rajgor@gmail.com>
2016-12-02 17:40:36 +0530

    Fixed issue of s3

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,20 +59,26 @@ class User < ActiveRecord::Base
 
 =end
 
-has_attached_file :profile_image,
-                            s3_region: 'us-west-2',
-                            storage: :s3,
-                            s3_protocol: :https,
-                            s3_credentials:  "#{Rails.root}/config/amazon_s3.yml",
-                            url: ':s3_host_name',
-                            path:  '/profile_image/:id/:filename',
-                            s3_host_name: 'https://s3-us-west-2.amazonaws.com/',
-                            :styles => {
-                              :preview => ["150x150>",:jpg],
-                              :medium => ["260x260#",:jpg],
-                              :large => ["100%", :medium] },
-                            :default_style => :thumb,
-                            :default_url => "/assets/missing.png"
+# has_attached_file :profile_image,
+#                             s3_region: 'us-west-2',
+#                             storage: :s3,
+#                             s3_protocol: :https,
+#                             s3_credentials:  "#{Rails.root}/config/amazon_s3.yml",
+#                             url: ':s3_host_name',
+#                             path:  '/profile_image/:id/:filename',
+#                             s3_host_name: 'https://s3-us-west-2.amazonaws.com/'
+
+  has_attached_file :profile_image,
+    s3_region: 'ap-northeast-1',
+    storage: :s3,
+    s3_protocol: :https,
+    s3_credentials:  "#{Rails.root}/config/amazon_s3.yml",
+    url: ':s3_domain_url',
+    path:  '/images/:id/:filename',
+    s3_host_alias: 'https://s3-ap-northeast-1.amazonaws.com/'
+
+    
+
 
   # has_attached_file :profile_image,
   #     s3_region: 'us-west-2',
