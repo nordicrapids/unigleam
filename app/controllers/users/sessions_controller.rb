c7190b383e142078b4a06dd8ac2a08700a83ffec
dharin <dharin.rajgor@gmail.com>
2016-12-12 11:06:31 +0530

    set the login email case sensitive

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -7,9 +7,9 @@ class Users::SessionsController < ApplicationController
   def create
     @status = false
     if params[:user][:email].include?('.co')
-      resource = User.find_for_database_authentication(email: params[:user][:email])
+      resource = User.find_for_database_authentication(email: params[:user][:email].downcase)
     else
-      resource = User.find_for_database_authentication(username: params[:user][:email])
+      resource = User.find_for_database_authentication(username: params[:user][:email].downcase)
     end
 
     if resource.valid_password?(params[:user][:password])
@@ -25,7 +25,7 @@ class Users::SessionsController < ApplicationController
   end
 
   def check_email
-    @user = User.find_by_email(params[:user][:email]) || User.find_by_username(params[:user][:email])
+    @user = User.find_by_email(params[:user][:email].downcase) || User.find_by_username(params[:user][:email])
     respond_to do |format|
      format.json { render :json => @user.present? }
     end
@@ -33,7 +33,7 @@ class Users::SessionsController < ApplicationController
 
   def check_email_registration
     if params[:user][:email].present?
-      @user = User.find_by_email(params[:user][:email])
+      @user = User.find_by_email(params[:user][:email].downcase)
     elsif params[:user][:username]
       @user = User.find_by_username(params[:user][:username])
     end
